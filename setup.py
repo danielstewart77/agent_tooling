eee9966a9d0160d3fa2812e4cc06553f3d82450a
Daniel Stewart <daniel.stewart77@gmail.com>
2025-03-13 11:20:40 -0500

    1) updated exposed functions 2) updated version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='agent_tooling',
-    version='0.2.0',
+    version='0.2.1',
     author='Daniel Stewart',
     author_email='daniel.stewart77@gmail.com',
     description='A lightweight tool registry for function metadata management',
