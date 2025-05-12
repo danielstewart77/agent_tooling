58d9e6009d33ce8b4e897a5691182e3cf35aa08e
Daniel Stewart <daniel.stewart77@gmail.com>
2025-05-12 10:47:57 -0500

    chore: bump version to 0.4.4 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='agent_tooling',
-    version='0.4.3',
+    version='0.4.4',
     author='Daniel Stewart',
     author_email='daniel.stewart77@gmail.com',
     description='A lightweight tool registry for function metadata management',
